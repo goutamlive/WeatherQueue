0d9e964b53a509668f10e7d584cbca33cec34ea0
Goutam Kumar <goukumar@deloitte.com>
2018-02-27 09:36:02 +0530

    Added concurrency in listener side

--- src/main/java/com/gom/weatherqueue/config/WorkerQueueConfig.java
+++ src/main/java/com/gom/weatherqueue/config/WorkerQueueConfig.java
@@ -8,14 +8,26 @@ import com.gom.weatherqueue.agent.Supervisor;
 import com.gom.weatherqueue.agent.Worker;
 import com.gom.weatherqueue.queue.WorkerQueue;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WorkerQueueConfig {
+
     @Bean
     public WorkerQueue workerQueue() {
         return new WorkerQueue("worker.queue");
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setConcurrentConsumers(3);
+        factory.setMaxConcurrentConsumers(10);
+        return factory;
     }
 
     @Bean
